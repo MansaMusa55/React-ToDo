b644be96ce246944da0d4440d113763e44d20ce4
Samuel <musax025@gmail.com>
2021-10-12 14:36:18 -0500

    Delete and complete button

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           todos={todos} 
           setTodos={setTodos} 
           setInputText={setInputText} />
-    <TodoList todos={todos} />
+    <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
